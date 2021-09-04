f1619d412a784e2e7dd4348f07218275dd3b12d7
Do-code-ing <zxcvbn658@naver.com>
2021-09-05 01:50:37 +0900

    2021-09-05

--- indeed.py
+++ indeed.py
@@ -5,18 +5,17 @@ LIMIT = 50
 URL = f"http://indeed.com/jobs?q=python&limit={LIMIT}"
 
 
-def extract_indeed_pages():
+def get_last_page():
     result = requests.get(URL)
     soup = BeautifulSoup(result.text, "html.parser")
-
     pagination = soup.find("div", {"class": "pagination"})
     links = pagination.find_all("a")
     pages = []
-
     for link in links[:-1]:
         pages.append(int(link.string))
 
     last_page = pages[-1]
+
     return last_page
 
 
@@ -32,7 +31,7 @@ def extract_job(soup):
     return {"title": title, "company": company, "location": location, "link": f"https://www.indeed.com/viewjob?jk={job_id}"}
 
 
-def extract_indeed_jobs(last_pages):
+def extract_jobs(last_pages):
     jobs = []
     for page in range(last_pages):
         print(f"Scrapping page {page}")
@@ -43,4 +42,10 @@ def extract_indeed_jobs(last_pages):
             job = extract_job(result)
             jobs.append(job)
 
+    return jobs
+
+
+def get_jobs():
+    last_page = get_last_page()
+    jobs = extract_jobs(last_page)
     return jobs
---
+++ so.py
@@ -0,0 +1,29 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = f"https://stackoverflow.com/jobs?q=python"
+
+
+def get_last_page():
+    result = requests.get(URL)
+    soup = BeautifulSoup(result.text, "html.parser")
+    pages = soup.find("div", {"class": "s-pagination"}).find_all("a")
+    last_page = pages[-2].get_text(strip=True)
+    return int(last_page)
+
+
+def extract_jobs(last_page):
+    jobs = []
+    for page in range(1, last_page+1):
+        result = requests.get(f"{URL}&pg={page}")
+        soup = BeautifulSoup(result.text, "html.parser")
+        results = soup.find_all("div", {"class": "-job"})
+        for result in results:
+            print(result["data-jobid"])
+    return
+
+
+def get_jobs():
+    last_page = get_last_page()
+    jobs = extract_jobs(last_page)
+    return jobs
